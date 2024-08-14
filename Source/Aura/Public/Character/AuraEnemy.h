c54594a2d103dc38a6c755298933047411b4d3bb
haperkk <1473444171@qq.com>
2024-08-14 14:43:11 +0800

    Ghoul Enemy

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -49,7 +49,7 @@ public:
 	UPROPERTY(BlueprintReadOnly, Category = "Combat")
 	bool bHitReacting = false;
 
-	UPROPERTY(BlueprintReadOnly, Category = "Combat")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
 	float BaseWalkSpeed = 250.f;
 
 	UPROPERTY(BlueprintReadOnly, BlueprintReadOnly, Category = "Combat")
